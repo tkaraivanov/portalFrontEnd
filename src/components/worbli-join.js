cf3f87cd634d124b0baf5700de48d7d053eb1fa1
Amit Rathi <amit.rathi.k@gmail.com>
2018-10-26 23:46:52 -0400

    added validation to register form

--- src/components/worbli-join.js
+++ src/components/worbli-join.js
@@ -77,8 +77,8 @@ class WorbliJoin extends PolymerElement {
 			<p>WORBLI is the place to access smarter financial services</p>
 			<form>
 				<input type="text" class="text" placeholder="Email Address" id="email" required>
-				<label><input type="checkbox" name="checkbox" value="value"> I agree to the <span><a href="/terms/">Terms</a></span> and <span><a href="/privacy/">Privacy Policy</a></span></label></br>
-				<label><input type="checkbox" name="checkbox" value="value"> I'm happy to recieve marketing communications from WORBLI</label></br></br>
+				<label><input type="checkbox" name="checkbox" value="value" required> I agree to the <span><a href="/terms/">Terms</a></span> and <span><a href="/privacy/">Privacy Policy</a></span></label></br>
+				<label><input type="checkbox" name="checkbox" value="value" required> I'm happy to recieve marketing communications from WORBLI</label></br></br>
 				<button class="btn-critical" on-click="_sendEmail">Join</button>
 			</form>
             <div class="center">Already on WORBLI? <span on-click="_signIn">Log In</span></div>
